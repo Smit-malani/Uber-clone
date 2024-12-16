e61fff67a0040dd65bc49fec0912783df965db2a
Smit-malani <malanismit41@gmail.com>
2024-12-16 13:34:38 +0530

    Captain Home Page Done

---
+++ Frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import profile from "../assets/profile.jpg"
+
+
+function CaptainDetails(){
+    return(
+        <>
+            <div className="lg:px-5 lg:pt-3 flex items-center justify-between gap-5 ">
+                <div className="flex gap-3 items-center">
+                    <div className="h-12 w-12 rounded-full overflow-hidden">
+                        <img className="w-full h-full object-cover" src={profile} alt="UserImge" />
+                    </div>
+                    <div>
+                        <h3 className="font-semibold text-lg">Jeremiah Curties</h3>
+                        <p className="text-sm opacity-70">Basic Level</p>
+                    </div>
+                </div>
+                <div className="text-right">
+                    <h4 className="font-bold">₹295.20</h4>
+                    <p className=" text-sm font-medium opacity-80">Earned</p>
+                </div>
+            </div>
+            <div className="py-5 flex items-center justify-around mt-3 bg-gray-100 rounded-lg">
+                <div className="text-center">
+                    <i className="ri-time-line text-3xl"></i>
+                    <h5 className="text-lg font-medium mt-2">10.2</h5>
+                    <p className="text-sm opacity-75">Hours Online</p>
+                </div>
+                <div className="text-center">
+                    <i className="ri-speed-up-line text-3xl"></i>
+                    <h5 className="text-lg font-medium mt-2">30KM</h5>
+                    <p className="text-sm opacity-75">Total Distance</p>
+                </div>
+                <div className="text-center">
+                    <i className="ri-booklet-line text-3xl"></i>
+                    <h5 className="text-lg font-medium mt-2">20</h5>
+                    <p className="text-sm opacity-75">Total Jobs</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CaptainDetails
--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { Profiler } from "react";
+import map_temp from "../assets/map_temp.gif"
+import Uber_logo_home_page from "../assets/Uber-logo-home-page.png"
+
+import { Link } from "react-router-dom";
+import CaptainDetails from "../components/CaptainDetails";
+
 
 function CaptainHome(){
     return(
-        <div>
-            captain home
-        </div>
+        <>
+            <div className="h-screen lg:w-screen lg:flex lg:h-screen ">
+                <div className="fixed top-2 flex px-3 w-full justify-between items-center lg:w-1/2">
+                    <img className="w-16" src={Uber_logo_home_page} alt="UBER" /> 
+                    <Link to='/captain-login' className="bg-white h-10 w-10 flex items-center justify-center rounded-full">
+                        <i className="ri-logout-box-r-line text-xl "></i>
+                    </Link>
+                </div>
+                <div className="h-3/5 lg:w-1/2 lg:h-full ">
+                    <img  className="h-full w-full lg:object-cover object-cover " src={map_temp} alt="unabele to load map" />
+                </div>
+                <div className="h-2/5 lg:w-1/2 lg:h-full lg:flex lg:flex-col flex flex-col gap-3  lg:justify-center px-3">
+                    <div className="flex items-center  justify-center w-full pt-2">
+                        <i className="ri-arrow-down-s-line lg:text-2xl opacity-80"></i>
+                    </div>
+                    <CaptainDetails/>
+                </div>
+
+            </div>
+        </>
     )
 }
 
